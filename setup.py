0275537d3b76eea3d0bc550448c67e7f79b54e28
atul <abharadwajj@gmail.com>
2021-12-16 22:39:47 +0530

    dq

--- setup.py
+++ setup.py
@@ -26,6 +26,6 @@ setuptools.setup(
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
     install_requires=[
-        "numpy","matplotlib","pandas"
+        "numpy","matplotlib","pandas","joblib"
     ]
 )
